df6a5fcb773f2f87e51ffa60a9904195731ce1c3
angeleduiglesias <angeledu.igle.22@gmail.com>
2025-03-23 23:22:26 -0500

    Error Btn.ts arreglado

--- src/app/components/btn-comienza/btn-comienza.component.ts
+++ src/app/components/btn-comienza/btn-comienza.component.ts
@@ -11,6 +11,6 @@ export class BtnComienzaComponent {
   @Output() mostrarFormulario = new EventEmitter<void>();
 
   showForm() {
-    this.mostrarFormulario.();
-  }emit
+    this.mostrarFormulario.emit();
+  }
 }
